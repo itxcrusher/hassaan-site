ccab34c29c50e8a15fa639b4f04c27eea3c7178c
Hassaan <muhammadhassaanjaved99@gmail.com>
2025-01-29 10:24:52 -0500

    Add Contact Animation

--- js/emailjs-form.js
+++ js/emailjs-form.js
@@ -1,20 +1,53 @@
-(function() {
-    // https://dashboard.emailjs.com/admin/account
+(function () {
     emailjs.init({
-      publicKey: "XZB7nrR299U0EOFrp",
+        publicKey: "XZB7nrR299U0EOFrp",
     });
 })();
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+window.onload = function () {
+    const form = document.getElementById("contact-form");
+    const formContainer = document.getElementById("form-container");
+    const overlay = document.getElementById("feedback-overlay");
+    const canvas = document.getElementById("feedback-canvas");
+
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
-        // https://dashboard.emailjs.com/admin
-        // https://dashboard.emailjs.com/admin/templates
-        emailjs.sendForm('service_fmz6mxd', 'template_3brgp3p', this)
+        formContainer.classList.add("processing");
+        overlay.style.display = "flex";
+
+        emailjs
+            .sendForm("service_fmz6mxd", "template_3brgp3p", form)
             .then(() => {
-                console.log('SUCCESS!');
-            }, (error) => {
-                console.log('FAILED...', error);
+                console.log("SUCCESS!");
+                loadAnimation(
+                    "https://lottie.host/4cb8169d-7231-4f2f-b93f-bb7e74c4d508/fVrcT66ihx.lottie"
+                );
+                form.reset();
+                setTimeout(() => {
+                    overlay.style.display = "none";
+                    formContainer.classList.remove("processing");
+                }, 6500);
+            })
+            .catch((error) => {
+                console.error("FAILED...", error);
+                loadAnimation(
+                    "https://lottie.host/embed/68dbbd9d-3009-4719-8b44-748855ba9535/aX6YS86frQ.lottie"
+                );
+                setTimeout(() => {
+                    overlay.style.display = "none";
+                    formContainer.classList.remove("processing");
+                }, 6500);
             });
     });
-}
+    
+    function loadAnimation(src) {
+        import("https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm").then(({ DotLottie }) => {
+            new DotLottie({
+                autoplay: true,
+                loop: false,
+                canvas: canvas,
+                src: src,
+            });
+        });
+    }
+};
